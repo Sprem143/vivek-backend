7bdc947fb885d1a04556fabc6cfc81fe4c889e71
Prem Kumar <prem68265@gmail.com>
2025-03-18 22:14:00 +0530

    db fix

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 
-const db= async()=>{
-    await mongoose.connect('mongodb://127.0.0.1:27017/vivek')
-    .then(()=> console.log("Localhost Database cannected"))
-    .catch((err)=> console.log(err))
-}
-
-
 // const db= async()=>{
-//     await mongoose.connect('mongodb+srv://Gstar:Gstar1456@cluster0.ks8az.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-//     .then(()=> console.log("Live Database cannected"))
+//     await mongoose.connect('mongodb://127.0.0.1:27017/vivek')
+//     .then(()=> console.log("Localhost Database cannected"))
 //     .catch((err)=> console.log(err))
 // }
+
+
+const db= async()=>{
+    await mongoose.connect('mongodb+srv://prem:Prem7366@website.6bysi.mongodb.net/?retryWrites=true&w=majority&appName=Website')
+    .then(()=> console.log("Live Database cannected"))
+    .catch((err)=> console.log(err))
+}
 
 module.exports= db;
